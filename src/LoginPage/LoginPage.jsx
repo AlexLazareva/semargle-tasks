55bdd8f4bfcc7aef245e8b94044b501b4a8029c4
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-12 15:09:35 +0300

    add base styles

--- src/LoginPage/LoginPage.jsx
+++ src/LoginPage/LoginPage.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const LoginPage = React.createClass({
+    handleLogIn() {
+        console.log('Login clicked');
+    },
+
     render() {
         return (
             <div className="loginPage">
