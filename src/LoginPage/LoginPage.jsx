8ce69ae617c260833322211fcf94999a7803d3a1
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-12 14:45:40 +0300

    create App and LoginPage

---
+++ src/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LoginPage = React.createClass({
+    render() {
+        return (
+            <div className="loginPage">
+                Login Page
+            </div>
+        );
+    }
+});
+
+export default LoginPage;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+
+import App from './App.jsx';
+import LoginPage from './LoginPage/LoginPage.jsx';
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path='/' component={App}>
+            <Route path='/login' component={LoginPage} />
+        </Route>
+    </Router>,
+    document.getElementById('mount-point')
+);
 
