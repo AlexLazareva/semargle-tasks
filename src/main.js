93c55b14436c44ec921e63248d4f4777e5b1f5ae
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-13 13:40:56 +0300

    create Login redirect

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 
 import SessionActions from './actions/SessionActions';
+import SessionStore from './actions/SessionStore';
 
 import App from './App.jsx';
+import LoggedInLayout from './containers/LoggedInLayout/LoggedInLayout';
 import LoginPage from './pages/LoginPage/LoginPage.jsx';
 import AboutPage from './pages/AboutPage/AboutPage.jsx';
 
@@ -17,11 +19,22 @@ function renderApp() {
         <Router history={hashHistory}>
             <Route path='/' component={App}>
                 <Route path='/login' component={LoginPage} />
-                <Route path='/about' component={AboutPage} />
+                <Route component={LoggedInLayout} onEnter={requireAuth}>
+                    <Route path='/about' component={AboutPage} />
+                </Route>
             </Route>
         </Router>,
         document.getElementById('mount-point')
     );
+}
+
+function requireAuth(nextState, replace) {
+    if (!SessionStore.isLoggedIn()) {
+        replace({
+            pathname: '/login',
+            state: { nextPathname: nextState.location.pathname }
+        });
+    }
 }
 
 
