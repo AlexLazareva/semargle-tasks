6c4e2eae1b2014192aa4f9da42d49d8a8dcf2dcf
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-14 20:55:22 +0300

    create TasksAction

--- src/actions/TasksActions.js
+++ src/actions/TasksActions.js
@@ -2,6 +2,23 @@ import AppDispatcher from './../dispatcher/AppDispatcher';
 import AppConstants from './../constants/AppConstants';
 import api from './../api';
 
-const TasksActions = {};
+const TasksActions = {
+    loadTasks(taskListId) {
+        api.listTasks(taskListId)
+        .then(data => {
+            console.log(data);
+            AppDispatcher.dispatch({
+                type: AppConstants.TASKS_LOAD_SUCCESS,
+                items: data.items || []
+            });
+        })
+        .catch(err => {
+            AppDispatcher.dispatch({
+                type: AppConstants.TASKS_LOAD_FAIL,
+                error: err
+            });
+        });
+    }
+};
 
 export default TasksActions;
