5eb2ba4cf75347eb5bf651d6848aaa4c0d65c34d
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-14 18:49:25 +0300

    add TasksStore to TasksPage

---
+++ src/actions/TasksActions.js
@@ -0,0 +1,7 @@
+import AppDispatcher from './../dispatcher/AppDispatcher';
+import AppConstants from './../constants/AppConstants';
+import api from './../api';
+
+const TasksActions = {};
+
+export default TasksActions;
--- src/pages/TasksPage/TasksPage.jsx
+++ src/pages/TasksPage/TasksPage.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import TasksStore from './../../stores/TasksStore';
+import TasksActions from './../../actions/TasksActions';
+
 import IconButton from 'material-ui/lib/icon-button';
 import ContentAdd from 'material-ui/lib/svg-icons/content/add';
 import './styles.less';
 
+function getStateFromFlux() {
+    return {
+        tasks: TasksStore.getTasks()
+    };
+}
+
 const TasksPage = React.createClass({
+    getInitialState() {
+        return {
+            ...getStateFromFlux()
+        } ;
+    },
+
     render() {
         return (
             <div className="taskPage">
