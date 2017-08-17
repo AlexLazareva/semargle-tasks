dffd677ef1f0ee192a7a50b18fc5a28f475fe103
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-08-17 19:41:03 +0300

    add editTask input

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -4,6 +4,8 @@ import IconMenu from 'material-ui/lib/menus/icon-menu';
 import MenuItem from 'material-ui/lib/menus/menu-item';
 import IconButton from 'material-ui/lib/icon-button';
 import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import RaisedButton from 'material-ui/lib/raised-button';
+import FlatButton from 'material-ui/lib/flat-button';
 
 import './styles.less';
 
@@ -36,22 +38,38 @@ const Task = React.createClass({
 
     render() {
         return (
-            <div className='task'>
-                <Checkbox
-                    className='task__checkbox'
-                    checked={this.props.isCompleted}
-                    onCheck={this.handleCheck}
-                />
-
-                <div className='task__text' onClick={this.handleEdit}>
-                    <div className='task__title'>{ this.props.text }</div>
+            this.state.isEditing
+            ?
+                <div className='task editing'>
+                    <input
+                        className='task__input'
+                        type='text'
+                        ref={ c => this.input = c }
+                    />
+                    <div className='task__toolbar'>
+                        <div>
+                            <RaisedButton primary onClick={this.handleSave} label='Save'/>
+                            <FlatButton onClick={this.handleCancel} label='Cancel' />
+                        </div>
+                    </div>
                 </div>
+            :
+                <div className='task'>
+                    <Checkbox
+                        className='task__checkbox'
+                        checked={this.props.isCompleted}
+                        onCheck={this.handleCheck}
+                    />
 
-                <IconMenu iconButtonElement={<IconButton><MoreVertIcon/></IconButton>}>
-                    <MenuItem onClick={this.handleEdit}>Edit</MenuItem>
-                    <MenuItem>Delete</MenuItem>
-                </IconMenu>
-            </div>
+                    <div className='task__text' onClick={this.handleEdit}>
+                        <div className='task__title'>{ this.props.text }</div>
+                    </div>
+
+                    <IconMenu iconButtonElement={<IconButton><MoreVertIcon/></IconButton>}>
+                        <MenuItem onClick={this.handleEdit}>Edit</MenuItem>
+                        <MenuItem>Delete</MenuItem>
+                    </IconMenu>
+                </div>
         );
     }
 });
