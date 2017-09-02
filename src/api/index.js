bc0cea7dc7bcb4a1127b61bab7390061409ba234
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-09-02 12:10:58 +0300

    replace config

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const CLIENT_ID = '102847238487-d06lsnps8kdm1a98vo2b8f4u7269n0pu.apps.googleusercontent.com';
+import { clientId } from './../config';
 const SCOPES = ['https://www.googleapis.com/auth/tasks', 'https://www.googleapis.com/auth/plus.me'];
 
 export default {
@@ -6,7 +6,7 @@ export default {
         return new Promise((resolve, reject) => {
             gapi.auth.authorize(
                 {
-                    'client_id': CLIENT_ID,
+                    'client_id': clientId,
                     'scope': SCOPES,
                     'immediate': params.immediate,
                     'cookie_policy': 'single_host_origin'
